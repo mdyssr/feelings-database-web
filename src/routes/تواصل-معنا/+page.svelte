d89938b5c6834cb6c14405117bfeee66b6f16ad2
mdyssr <mhmmdyssraamr@gmail.com>
2023-08-11 00:29:01 +0300

    final prototype 1

--- src/routes/تواصل-معنا/+page.svelte
+++ src/routes/تواصل-معنا/+page.svelte
@@ -1,1 +1,58 @@
-<h1>اتصل بنا</h1>
+<div class="container">
+  <h3>يمكنك التواصل معنا ومشاركة مشاعرك من خلال الروابط التالية:</h3>
+  <ul>
+    <li>
+      ديسكورد 👈 : <a href="https://discord.com" target="_blank">اضفط هنا</a>
+    </li>
+    <li>
+      فيسبوك 👈 : <a href="https://facebook.com" target="_blank">اضفط هنا</a>
+    </li>
+  </ul>
+
+  <h3>أو يمكنك التواصل معي على حساباتي الشخصية من هنا:</h3>
+  <ul>
+    <li>
+      انستجرام 👈 : <a href="https://instagram.com/mdyss.r" target="_blank"
+        >اضفط هنا</a
+      >
+    </li>
+    <li>
+      فيسبوك 👈 : <a href="https://facebook.com" target="_blank">اضفط هنا</a>
+    </li>
+  </ul>
+</div>
+
+<style lang="scss">
+  .container {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 1rem;
+  }
+
+  h3 {
+    padding: 0;
+    margin: 0;
+  }
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    // margin: 0;
+    padding: 0;
+  }
+
+  li {
+    background-color: #333;
+    color: wheat;
+    font-size: 2rem;
+    padding: 1rem;
+    margin: 0;
+  }
+
+  a {
+    color: wheat;
+    font-size: 2rem;
+  }
+</style>
